5645c0480dedb7db55200a26442833708f61e8eb
DanielGhanabari <danielghanbari7@gmail.com>
2021-07-11 00:42:07 +0100

    added cards component

--- cyf-countries-react/src/App.js
+++ cyf-countries-react/src/App.js
@@ -1,22 +1,13 @@
-import data from "./countriesAll.json";
 import "./App.css";
+import Cards from "./Components/Cards";
 
 
 const App = () => {
 
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
-
+ 
   return (
     <div className = "container">
-      {data.map((x)=><ul key={x.index} className="bg-light p-2">
-        <li><img src={x.flag} alt="countries flag"></img></li>
-        <li className="pb-3 pt-2 font-weight-bold">{x.name} </li>
-        <li>Population: {numberWithCommas(x.population)}</li>
-        <li>Region: {x.region}</li>
-        <li>Capital: {x.capital}</li>
-        </ul>)}
+     <Cards />
     </div>
   );
 }
---
+++ cyf-countries-react/src/Components/Cards.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import data from ".././countriesAll.json";
+
+
+const Cards = () => {
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    }
+    return (
+        <div>
+               {data.map((x)=><ul key={x.index} className="bg-light p-2">
+        <li><img src={x.flag} alt="countries flag"></img></li>
+        <li className="pb-3 pt-2 font-weight-bold">{x.name} </li>
+        <li>Population: {numberWithCommas(x.population)}</li>
+        <li>Region: {x.region}</li>
+        <li>Capital: {x.capital}</li>
+        </ul>)}
+            
+        </div>
+    )
+}
+
+export default Cards
