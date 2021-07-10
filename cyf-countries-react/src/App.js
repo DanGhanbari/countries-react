60c494a43303fa674ee02701fa36df64912ffd61
DanielGhanabari <danielghanbari7@gmail.com>
2021-07-10 20:38:01 +0100

    css style and flags added

--- cyf-countries-react/src/App.js
+++ cyf-countries-react/src/App.js
@@ -1,4 +1,5 @@
 import data from "./countriesAll.json";
+import "./App.css";
 
 
 const App = () => {
@@ -8,8 +9,14 @@ const App = () => {
 }
 
   return (
-    <div>
-      {data.map((x)=><ul key={x.index}><li>{x.name} </li><li>{numberWithCommas(x.population)}</li></ul>)}
+    <div className = "container">
+      {data.map((x)=><ul key={x.index} className="bg-light p-2">
+        <li><img src={x.flag} alt="countries flag"></img></li>
+        <li className="pb-3 pt-2 font-weight-bold">{x.name} </li>
+        <li>Population: {numberWithCommas(x.population)}</li>
+        <li>Region: {x.region}</li>
+        <li>Capital: {x.capital}</li>
+        </ul>)}
     </div>
   );
 }
